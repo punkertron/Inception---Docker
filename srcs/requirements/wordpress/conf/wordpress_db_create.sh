64dbc664b7b78d4375a6774e8ad6cc646a696006
punkertron <vascherbatov@mail.ru>
2023-06-24 14:49:58 +0400

    for save

--- srcs/requirements/wordpress/conf/wordpress_db_create.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-
-CREATE DATABASE wordpress;
-CREATE USER wordpress@localhost IDENTIFIED BY 'pass';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;
-FLUSH PRIVILEGES;
-
