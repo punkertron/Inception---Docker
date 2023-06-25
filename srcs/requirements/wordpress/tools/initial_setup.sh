7f591eeea7584f56d931f17e271425dc5dc59248
punkertron <vascherbatov@mail.ru>
2023-06-25 16:37:55 +0400

    for saving

--- srcs/requirements/wordpress/tools/initial_setup.sh
+++ srcs/requirements/wordpress/tools/initial_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+chmod -R 775 /var/www/html/wordpress;
+chown -R www-data /var/www/html/wordpress;
+mkdir -p /run/php/;
+touch /run/php/php7.4-fpm.pid
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 chmod +x wp-cli.phar;
 mv wp-cli.phar /usr/local/bin/wp;
@@ -7,14 +12,14 @@ mv wp-cli.phar /usr/local/bin/wp;
 chmod -R 775 /var/www/html/wordpress;
 
 wp core download --allow-root;
-rm /var/www/html/wordpress/wp-config-sample.php;
+#mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
 
-#wp core config --dbhost=mariadb --dbname=wordpress --dbuser=drohanne --dbpass=password --allow-root;
+#wp config set DB_NAME wordpress --allow-root;
+#wp config set DB_USER drohanne --allow-root;
+#wp config set DB_PASSWORD password --allow-root;
+#wp config set DB_HOST mariadb --allow-root; 
 
-
-pwd;
-ls;
 
 wp core install \
 	--url=drohanne.42.fr \
