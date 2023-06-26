f6a8892e32e1b82e3af5c109856c81b029e74c8d
punkertron <vascherbatov@mail.ru>
2023-06-26 14:18:55 +0400

    it works! todo a lot of stuff

--- srcs/requirements/wordpress/tools/initial_setup.sh
+++ srcs/requirements/wordpress/tools/initial_setup.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-chmod -R 775 /var/www/html/wordpress;
-chown -R www-data /var/www/html/wordpress;
-mkdir -p /run/php/;
-touch /run/php/php7.4-fpm.pid
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-chmod +x wp-cli.phar;
-mv wp-cli.phar /usr/local/bin/wp;
-
-chmod -R 775 /var/www/html/wordpress;
-
-wp core download --allow-root;
-#mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-
-
-#wp config set DB_NAME wordpress --allow-root;
-#wp config set DB_USER drohanne --allow-root;
-#wp config set DB_PASSWORD password --allow-root;
-#wp config set DB_HOST mariadb --allow-root; 
-
+chmod -R 775 /var/www/html/wordpress
 
 wp core install \
 	--url=drohanne.42.fr \
@@ -27,15 +12,14 @@ wp core install \
 	--admin_user=root \
 	--admin_password=strongpassword \
 	--admin_email=drohanne@student.42yerevan.am \
-	--allow-root;
-	#--path=/var/www/html/wordpress;
+	--allow-root
 
 wp user create Vladislav vladislav@example.com \
 	--usserpass=password \
        	--role=author \
-	--allow-root ;
-	#--path=/var/www/html/wordpress;
+	--allow-root 
 
-wp theme install twentytwenty-three --activate \
-	--allow-root;
-	#--path=/var/www/html/wordpress;
+#wp theme install twentytwenty-three --activate \
+#	--allow-root
+
+php-fpm7.4 -F
