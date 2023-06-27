1c8b847dc87a9c4430592a7d075f4af92b156af5
punkertron <vascherbatov@mail.ru>
2023-06-27 16:34:32 +0400

    mariadb works

--- srcs/requirements/mariadb/tools/initial.sh
+++ srcs/requirements/mariadb/tools/initial.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+service mariadb start
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" > createdb.sql
 echo "CREATE USER IF NOT EXISTS '${DB_USER}' IDENTIFIED BY '${DB_PASS}';" >> createdb.sql
 echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}';" >> createdb.sql
 echo "FLUSH PRIVILEGES;" >> createdb.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';" >> createdb.sql
 
-service mariadb start
 mariadb < createdb.sql
 rm createdb.sql
-
 mysqladmin shutdown -p${ROOT_PASS}
 
 /usr/bin/mysqld_safe
