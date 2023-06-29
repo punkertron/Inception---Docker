0b272a3762bd637060a345afa10235e0f4819957
punkertron <vascherbatov@mail.ru>
2023-06-29 15:29:41 +0400

    ftp works

--- srcs/bonus/ftp/tools/initial.sh
+++ srcs/bonus/ftp/tools/initial.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-adduser ftpuser
-echo "ftpuser:ftppass" | chpasswd
-chown -R ftpuser:ftpuser /var/html/wordpress
+useradd $FTP_USER
+echo "$FTP_USER:$FTP_PASS" | chpasswd
+#&> dev/null
+echo $FTP_USER > /etc/vsftpd.userlist
+mkdir -p /home/$FTP_USER/ftp
+
+echo "user_sub_token=$FTP_USER
+local_root=/home/$FTP_USER/ftp" >> /etc/vsftpd.conf
 
 /usr/sbin/vsftpd /etc/vsftpd.conf
