7f9d16eec79050301432b1f65c5e11d20a2f4c02
punkertron <vascherbatov@mail.ru>
2023-06-30 14:03:11 +0400

    done?

--- srcs/bonus/ftp/tools/initial.sh
+++ srcs/bonus/ftp/tools/initial.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-useradd $FTP_USER
-echo "$FTP_USER:$FTP_PASS" | chpasswd
-#&> dev/null
-echo $FTP_USER > /etc/vsftpd.userlist
-mkdir -p /home/$FTP_USER/ftp
+if [ ! -e "/etc/vsftpd.userlist" ]; then
+	useradd $FTP_USER
+	echo "$FTP_USER:$FTP_PASS" | chpasswd
+	echo $FTP_USER > /etc/vsftpd.userlist
+	mkdir -p /home/$FTP_USER/ftp
 
-echo "user_sub_token=$FTP_USER
-local_root=/home/$FTP_USER/ftp" >> /etc/vsftpd.conf
+	echo "user_sub_token=$FTP_USER
+	local_root=/home/$FTP_USER/ftp" >> /etc/vsftpd.conf
+fi
 
 /usr/sbin/vsftpd /etc/vsftpd.conf
--- srcs/requirements/wordpress/tools/initial_setup.sh
+++ srcs/requirements/wordpress/tools/initial_setup.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
+if [ ! -e "/var/www/html/wordpress/wp-config.php" ]; then
+	#core
+	wp core download --allow-root
 
-#core
-wp core download --allow-root
+	mv wp-config-sample.php wp-config.php
 
-mv wp-config-sample.php wp-config.php
+	wp config set DB_NAME $DB_NAME  --allow-root
+	wp config set DB_USER $DB_USER  --allow-root
+	wp config set DB_PASSWORD $DB_PASS  --allow-root
+	wp config set DB_HOST $WDB_HOST  --allow-root
 
-wp config set DB_NAME $DB_NAME  --allow-root
-wp config set DB_USER $DB_USER  --allow-root
-wp config set DB_PASSWORD $DB_PASS  --allow-root
-wp config set DB_HOST $WDB_HOST  --allow-root
+	wp core install \
+		--url=$W_URL \
+		--title=$W_TITLE \
+		--admin_user=$W_ADM_USER \
+		--admin_password=$W_ADM_PASS \
+		--admin_email=$W_ADM_MAIL \
+		--allow-root
 
-wp core install \
-	--url=$W_URL \
-	--title=$W_TITLE \
-	--admin_user=$W_ADM_USER \
-	--admin_password=$W_ADM_PASS \
-	--admin_email=$W_ADM_MAIL \
-	--allow-root
+	wp user create $W_USER $W_MAIL \
+		--user_pass=$W_PASS \
+	       	--role=$W_ROLE \
+		--allow-root 
 
-wp user create $W_USER $W_MAIL \
-	--user_pass=$W_PASS \
-       	--role=$W_ROLE \
-	--allow-root 
-
-wp theme install twentytwentythree --activate \
-	--allow-root
-
+	wp theme install twentytwentythree --activate --allow-root
 
 
-#redis
+	#redis
 
-chmod -R 777 wp-content
-chmod -R 777 wp-admin
+	chmod -R 777 wp-content
 
-wp plugin install redis-cache --activate --allow-root
+	wp plugin install redis-cache --activate --allow-root
 
-wp config set WP_REDIS_HOST $REDIS_HOST --allow-root
-wp config set WP_REDIS_PORT 6379 --allow-root
-wp config set WP_REDIS_DATABASE 0 --allow-root
-wp config set WP_REDIS_TIMEOUT 1 --allow-root
-wp config set WP_REDIS_READ_TIMEOUT 1 --allow-root
+	wp config set WP_REDIS_HOST $REDIS_HOST --allow-root
+	wp config set WP_REDIS_PORT 6379 --allow-root
+	wp config set WP_REDIS_DATABASE 0 --allow-root
+	wp config set WP_REDIS_TIMEOUT 1 --allow-root
+	wp config set WP_REDIS_READ_TIMEOUT 1 --allow-root
 
-wp redis enable --allow-root
+	wp redis enable --allow-root
+fi
 
 php-fpm7.4 -F
